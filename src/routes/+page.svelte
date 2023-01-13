9cf6e028f662af2e66577b9a5682024a4b2064e2
Martin Grubinger <martin.grubinger@gmail.com>
2023-01-13 14:03:01 +0100

    improve landingpage (slightly)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,7 @@
 <script>
-
+	let exampleURL = new URLSearchParams();
+	exampleURL.set('title', 'Nintendo Switch OLED schwarz/weiß');
+	exampleURL.set('image', 'https://gzhls.at/i/76/69/2557669-n0.jpg');
 </script>
 
 <svelte:head>
@@ -9,8 +11,30 @@
 
 <section>
 	<h1>hello, this is the GH og:image service</h1>
+
+	<p>
+		See <a href="https://github.com/Geizhals-Preisvergleich/geizhals-og-image"
+			>https://github.com/Geizhals-Preisvergleich/geizhals-og-image</a
+		> or README.md for more details.
+	</p>
+	<p>
+		<strong>Images:</strong> Visit <a href="/og/">/og/</a> for the generated images, e.g.
+		<a href="/og?{exampleURL.toString()}">/og?{exampleURL.toString()}</a>
+	</p>
+	<p>
+		<strong>Editor:</strong> Visit <a href="/og/editor/">/og/editor/</a> for the editor. e.g.
+		<a href="/og/editor?{exampleURL.toString()}">/og/editor?{exampleURL.toString()}</a>
+	</p>
 </section>
 
 <style lang="scss">
+	section {
+		padding: 1rem;
+		max-width: 600px;
+		margin: 0 auto;
+	}
 
+  * + * {
+    margin-top: 1rem;
+  }
 </style>
